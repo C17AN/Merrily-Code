866023a89b14b4117b7d724fa32cf3b962309a98
c17an <kimchanmin1@gmail.com>
2021-08-13 05:39:28 +0900

    style: 야간모드 배경색 변경

--- pages/_document.js
+++ pages/_document.js
@@ -34,7 +34,7 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
           />
         </Head>
-        <body className="antialiased text-black bg-white dark:bg-gray-900 dark:text-white">
+        <body className="antialiased text-black bg-white dark:bg-[#2c2c2e] dark:text-white">
           <Main />
           <NextScript />
         </body>
