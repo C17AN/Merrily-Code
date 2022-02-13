781608e38f541a839a138ee9981feab978a57963
chanmin, kim <kimchanmin1@gmail.com>
2022-02-13 16:43:18 +0900

    feat: apply bundle analyzer on project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const withPlugins = require('next-compose-plugins')
 const frontmatterRemarkPlugin = require('./frontmatter')
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const mdxPlugin = require('@next/mdx')({
   options: {
@@ -6,10 +10,9 @@ const mdxPlugin = require('@next/mdx')({
   },
 })
 
-// export the configuration
-module.exports = mdxPlugin({
+module.exports = withPlugins([mdxPlugin, withBundleAnalyzer], {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   images: {
     domains: ['drive.google.com', 'github.com']
-  }
+  },
 })
