65592c0f4dd1d7721447343e89898ef9e111d73b
chanmin, kim <kimchanmin1@gmail.com>
2022-02-19 01:21:49 +0900

    feat: change getCategoryDataList feature

--- lib/utils/getCategoryDataList.ts
+++ lib/utils/getCategoryDataList.ts
@@ -1,11 +1,16 @@
-import category from "data/category";
+import categoryData from "data/category";
 import Category from "type/Category";
 
 // TODO: Flatmap을 사용한 리팩터링
-const getCategoryDataList = (): Category[] => {
-  const categoryArray = Object.values(category);
-  return categoryArray.reduce((categoryList, mainCategory) => {
-    return categoryList.concat(mainCategory.map((subCategory) => subCategory));
+const getCategoryDataList = (): any => {
+  const categoryArray = Object.values(categoryData);
+
+  return categoryArray.reduce((categoryList: Category[], mainCategory) => {
+    if (Array.isArray(mainCategory)) {
+      return categoryList.concat(mainCategory.map((subCategory) => subCategory));
+    } else {
+      return categoryList.concat(mainCategory);
+    }
   }, []);
 };
 
