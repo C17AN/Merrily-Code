b3fa221145e985cdb7a8aaad24b7ece4b96c474e
chanmin, kim <kimchanmin1@gmail.com>
2022-03-17 11:38:06 +0900

    chore: De-index main portfolio page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import Portfolio from "components/Portfolio";
 import BaseLayout from "layout/BaseLayout";
+import Head from "next/head";
 import React from "react";
 
 interface Props {}
 
 const BasePage = (props: Props) => {
-  return <Portfolio />;
+  return (
+    <>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <Portfolio />
+    </>
+  );
 };
 
 export default BasePage;
