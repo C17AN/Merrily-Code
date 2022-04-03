d44c2611a9f5482f1f526d54d58aeae751ac2e3a
chanmin, kim <kimchanmin1@gmail.com>
2022-04-04 01:54:40 +0900

    fix: rollback main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
 import Home from "components/Home";
 import Head from "next/head";
-import React, { useState } from "react";
+import fs from "fs";
+import path from "path";
+import React, { createContext, useState } from "react";
+import matter from "gray-matter";
+import Post from "type/Post";
+import Portfolio from "components/Home/Portfolio";
 
-interface Props {}
+type BasePageProps = {
+  posts: Post[];
+};
 
-const BasePage = (props: Props) => {
+export const RecentPostContext = createContext<{ recentPostList: Post[] } | null>(null);
+
+const BasePage = ({ posts }: BasePageProps) => {
   const description = `코드와 음악, 그리고 맛있는 음식을 사랑하는 개발자입니다.`;
   const ogSiteName = "즐겁게, 코드";
+  const recentPosts = posts.slice(5);
+  const [recentPostList, setRecentPostList] = useState<Post[]>(recentPosts);
+
+  console.log(posts);
 
   return (
     <>
@@ -16,9 +29,31 @@ const BasePage = (props: Props) => {
         <meta property="og:site_name" content={ogSiteName} />
         <meta property="og:description" content={description} />
       </Head>
-      <Home />
+      {/* <RecentPostContext.Provider value={{ recentPostList }}>
+        <Home recentPosts={recentPosts} />
+      </RecentPostContext.Provider> */}
+      <Portfolio />
     </>
   );
 };
+
+export async function getStaticProps() {
+  const postsDirectory = path.join(process.cwd(), "data/posts");
+  const filenames = fs.readdirSync(postsDirectory);
+
+  const posts = filenames.map((filename) => {
+    const markdownWithMeta = fs.readFileSync(
+      path.join(process.cwd(), "data/posts", filename),
+      "utf-8"
+    );
+    const { data: frontMatter } = matter(markdownWithMeta);
+    return {
+      frontMatter,
+      filename: filename.split(".")[0],
+    };
+  });
+
+  return { props: { posts } };
+}
 
 export default BasePage;
